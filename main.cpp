303e53086c0346441edce05e9b92f48cc49ddf56
Gabriele Salvato <gabriele.salvato@cnr.it>
2017-04-21 13:13:32 +0200

    work in progress

--- main.cpp
+++ main.cpp
@@ -20,38 +20,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "scorecontroller.h"
 #include "choosediscilpline.h"
 #include <QApplication>
-#include <QSharedMemory>
 #include <QMessageBox>
 
 
 int
 main(int argc, char *argv[]) {
 
-#ifndef Q_OS_ANDROID
-  QSharedMemory lockingMemory;
-#endif
-
     QApplication app(argc, argv);
 
-    QFont myFont = QApplication::font();
-    myFont.setPointSize(32);
-    app.setFont(myFont, "QEdit");
-    myFont.setPointSize(18);
-    app.setFont(myFont, "QRadioButton");
-    app.setFont(myFont, "QLabel");
-
-#ifndef Q_OS_ANDROID
-    lockingMemory.setKey("ScoreController");
-    if(lockingMemory.attach()) {
-        QMessageBox::information(NULL, "Score Controller", "Esiste un'altra istanza attiva");
-        return 0;
-    }
-    if(!lockingMemory.create(1)) {
-        QMessageBox::information(NULL, "Score Controller", "Impossibile creare il lock");
-        lockingMemory.detach();
-        return 0;
-    }
-#endif
+//    QFont myFont = QApplication::font();
+//    myFont.setPointSize(32);
+//    app.setFont(myFont, "QEdit");
+//    myFont.setPointSize(18);
+//    app.setFont(myFont, "QRadioButton");
+//    app.setFont(myFont, "QLabel");
 
     ChooseDiscilpline chooser;
     chooser.exec();
@@ -64,10 +46,7 @@ main(int argc, char *argv[]) {
     w.showMaximized();
     //w.show();
 #endif
-    int iresult = app.exec();
 
-#ifndef Q_OS_ANDROID
-    lockingMemory.detach();
-#endif
+    int iresult = app.exec();
     return iresult;
 }
